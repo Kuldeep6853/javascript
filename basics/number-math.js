8350531e64874eb025b3c7a4a94f776348f7318b
Kuldeep singh <145250371+Kuldeep6853@users.noreply.github.com>
2024-06-15 05:15:38 +0000

    number-math

---
+++ basics/number-math.js
@@ -0,0 +1,41 @@
+const score =400
+console.log(score);
+
+const balance =  new Number(100)
+console.log(balance)
+
+console.log(balance.toString().length);
+console.log(balance.toFixed(2));
+
+const otherNumber = 23.8966
+
+console.log(otherNumber.toPrecision(3))
+
+const hunderds = 1000000
+console.log(hunderds.toLocaleString('en-IN'));
+
+
+
+//++++++++++++++++++++++++++++++ Math ++++++++++++++++++
+
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.4));
+// console.log(Math.ceil(4.8));
+// console.log(Math.floor(4.2));
+
+// console.log(Math.min(4, 9,5,3,6));
+// console.log(Math.max(4,0,34,5,3,2));
+
+
+console.log(Math.random());
+console.log((Math.random()*10) +1);
+
+// formula 
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min +1)) +10);
+
+
