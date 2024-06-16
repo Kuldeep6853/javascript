34b5d3005bab5bfce7d353ac891b0372babb5608
Kuldeep singh <145250371+Kuldeep6853@users.noreply.github.com>
2024-06-16 19:28:35 +0000

    object2

---
+++ basics/object2.js
@@ -0,0 +1,59 @@
+// const tinderUser = new Object()
+const tinderUser ={}
+
+
+
+tinderUser.id="123abc"
+tinderUser.name = "Sammy"
+tinderUser.isLoggedIn =false
+
+// console.log(tinderUser);
+
+const regularUser ={
+    email: "some@gmail.com",
+    fullname:{
+        userfullname:{
+            firstname:"hitesh",
+            lastname:"singh"
+        }
+    }
+}
+
+// console.log(regularUser.fullname.userfullname.firstname)
+
+const obj1 ={1:"a",2:"b"}
+const obj2 ={3:"a",4:"b"}
+
+// const obj3={obj1,obj2}
+// const obj3 = Object.assign({},obj1,obj2)
+
+const obj3 ={...obj1,...obj2}  //spread
+// console.log(obj3)
+
+// console.log(tinderUser);
+
+// console.log(Object.keys(tinderUser));
+// console.log(Object.values(tinderUser));
+// console.log(Object.entries(tinderUser));
+
+// console.log(tinderUser.hasOwnProperty('isLoggedIn'));
+
+
+const course ={
+    coursename:"js in hindi",
+    price:"999",
+    courseInstructore:"hitesh"
+}
+
+const{courseInstructore: instructore} =course
+
+// console.log(courseInstructore);
+
+console.log(instructore);
+
+
+// {
+//     "name":"hitesh",
+//    "coursename":"js in hindi"
+//     "price" : "free"
+// }
