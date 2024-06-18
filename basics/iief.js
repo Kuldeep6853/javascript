f8d18ac00d267d851cb90d585be86d037c8522a8
Kuldeep singh <145250371+Kuldeep6853@users.noreply.github.com>
2024-06-18 19:21:55 +0000

    new work in  iief

--- basics/iief.js
+++ basics/iief.js
@@ -1,1 +1,10 @@
 // Immediately Invoked Function Expression (IIFE)
+
+
+(function chai(){
+    console.log(`db gcvb`);
+}) ();
+
+( (name )=> {
+    console.log(`hello world ${name} `);
+}) ("cartoon")
