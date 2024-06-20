8169ad0674a61bc1589badb39ddcf65ff92ffe1a
Kuldeep singh <145250371+Kuldeep6853@users.noreply.github.com>
2024-06-20 07:21:02 +0000

    condition

---
+++ basics/conditional.js
@@ -0,0 +1,31 @@
+// falsy values
+// false , 0, -0, BigInt 0n,"",null, undefined,NaN
+
+//truthy value
+
+// "0", 'false', " ",[],{}, function(){}
+
+const userEmail= []
+if(userEmail.length === 0){
+    console.log("Array is empty");
+}
+
+const emptyObj ={}
+
+if(Object.keys(emptyObj).length === 0){
+    console.log("object is empty");
+}
+
+
+// Nullish Coalescing Opeartor (??): null undefined
+
+let val1;
+// val1= 5 ?? 10
+// val1 = null ?? 10
+
+
+
+
+
+
+console.log(val1);
